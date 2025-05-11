42a4dd35b4437548428dba558babb88087083063
AAO31 <aalvarezo@utec.edu.pe>
2025-05-11 13:29:15 -0500

    MyJetAnalyzerBeta.cc

--- MyJetAnalyzerBeta.cc
+++ MyJetAnalyzerBeta.cc
@@ -2,24 +2,24 @@
 #include <iostream>  // Para std::cout
 #include "FWCore/Framework/interface/Frameworkfwd.h" //componentes esenciales de CMSSW
 #include "FWCore/Framework/interface/EDAnalyzer.h" // construir analizador
-#include "FWCore/Framework/interface/Event.h" //para acceder la información de cada colisión
+#include "FWCore/Framework/interface/Event.h" //para acceder la informacion de cada colision
 #include "FWCore/Framework/interface/MakerMacros.h"  //  DEFINE_FWK_MODULE
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 // Definir Analyzer
 class MyJetAnalyzerBeta : public edm::EDAnalyzer {
 public:
-    explicit MyJetAnalyzerBeta(const edm::ParameterSet&); //Para leer configuraciones desde el archivo .cfg
+    explicit MyJetAnalyzerBeta(const edm::ParameterSet&); //Para leer configuraciones desde el archivo cfg
     void analyze(const edm::Event&, const edm::EventSetup&) override;
 };
 
-// Implementación del constructor
+// Implementacion del constructor
 MyJetAnalyzerBeta::MyJetAnalyzerBeta(const edm::ParameterSet& iConfig) {}
 
-// Implementación de analyze()
+// Implementacion de analyze()
 void MyJetAnalyzerBeta::analyze(const edm::Event& iEvent, const edm::EventSetup&) {
     std::cout << "Procesando evento: " << iEvent.id().event() << std::endl;
 }
 
-// Macro final (debe estar después de la definición completa de la clase)
+// Macro final (debe estar después de la definicion completa de la clase)
 DEFINE_FWK_MODULE(MyJetAnalyzerBeta);
