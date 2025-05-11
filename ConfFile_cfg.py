4b344497e5acde2c64fda32b1ed7c6e202b8b7bb
AAO31 <aalvarezo@utec.edu.pe>
2025-05-11 11:54:45 -0500

    ConfFile_cfg.py

--- ConfFile_cfg.py
+++ ConfFile_cfg.py
@@ -3,23 +3,14 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("JETANALYSIS")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10))
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        'file:input.root'
+        'file:input.root'  # Reemplazar con tu archivo
     )
 )
 
-process.myJetAnalyzerBeta = cms.EDAnalyzer('MyJetAnalyzerBeta',
-    jets = cms.InputTag("ak4PFJets"),
-    jetPtMin = cms.double(30.0),
-    jetEtaMax = cms.double(2.5),
-    jetAlgo = cms.string("AK4")
-)
+process.myJetAnalyzer = cms.EDAnalyzer('MyJetAnalyzerBeta')
 
-process.TFileService = cms.Service("TFileService",
-    fileName = cms.string('jetAnalysisOutput.root')
-)
-
-process.p = cms.Path(process.myJetAnalyzerBeta)
+process.p = cms.Path(process.myJetAnalyzer)
