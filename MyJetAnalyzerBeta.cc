b7135ebe95bed30297513467c98454d4e52cc108
AAO31 <aalvarezo@utec.edu.pe>
2025-05-11 13:01:18 -0500

    MyJetAnalyzerBeta.cc

--- MyJetAnalyzerBeta.cc
+++ MyJetAnalyzerBeta.cc
@@ -1,14 +1,15 @@
 // Includes esenciales
 #include <iostream>  // Para std::cout
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"  // ¡Necesario para DEFINE_FWK_MODULE!
+#include "FWCore/Framework/interface/Frameworkfwd.h" //componentes esenciales de CMSSW
+#include "FWCore/Framework/interface/EDAnalyzer.h" // construir analizador
+#include "FWCore/Framework/interface/Event.h" //para acceder la información de cada colisión
+#include "FWCore/Framework/interface/MakerMacros.h"  //  DEFINE_FWK_MODULE
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
+// Definir Analyzer
 class MyJetAnalyzerBeta : public edm::EDAnalyzer {
 public:
-    explicit MyJetAnalyzerBeta(const edm::ParameterSet&);
+    explicit MyJetAnalyzerBeta(const edm::ParameterSet&); //Para leer configuraciones desde el archivo .cfg
     void analyze(const edm::Event&, const edm::EventSetup&) override;
 };
 
