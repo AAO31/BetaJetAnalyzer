2abe20e32f5246fffc8514f924af29973a8148d5
AAO31 <aalvarezo@utec.edu.pe>
2025-05-11 12:06:53 -0500

    MyJetAnalyzerBeta.cc

--- MyJetAnalyzerBeta.cc
+++ MyJetAnalyzerBeta.cc
@@ -1,5 +1,9 @@
+// Includes esenciales
+#include <iostream>  // Para std::cout
+#include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"  // ¡Necesario para DEFINE_FWK_MODULE!
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 class MyJetAnalyzerBeta : public edm::EDAnalyzer {
@@ -8,11 +12,13 @@ public:
     void analyze(const edm::Event&, const edm::EventSetup&) override;
 };
 
+// Implementación del constructor
 MyJetAnalyzerBeta::MyJetAnalyzerBeta(const edm::ParameterSet& iConfig) {}
 
+// Implementación de analyze()
 void MyJetAnalyzerBeta::analyze(const edm::Event& iEvent, const edm::EventSetup&) {
-    // Solo imprime el número de evento como prueba
     std::cout << "Procesando evento: " << iEvent.id().event() << std::endl;
 }
 
+// Macro final (debe estar después de la definición completa de la clase)
 DEFINE_FWK_MODULE(MyJetAnalyzerBeta);
